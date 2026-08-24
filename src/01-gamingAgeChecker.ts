e195f1ea30d4c711d59ba027201dd42a351c764d
Aminul islam <aminulislam424842@gmail.com>
2026-08-24 22:12:23 +0600

    Add gaming age rating access checker

---
+++ src/01-gamingAgeChecker.ts
@@ -0,0 +1,18 @@
+function getAgeRatingAccess(age: number): string {
+  if (age < 7) {
+    return "E (Everyone)"
+  }
+  else if (age <= 12) {
+    return "E10+ (Everyone 10+)"
+  }
+  else if (age <= 16) {
+    return "T (Teen)"
+  } else {
+    return "M (Mature)"
+  }
+}
+
+console.log(getAgeRatingAccess(5))
+console.log(getAgeRatingAccess(10))
+console.log(getAgeRatingAccess(15))
+console.log(getAgeRatingAccess(20))
